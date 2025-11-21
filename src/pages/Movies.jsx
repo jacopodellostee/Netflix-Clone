09b89689b0571eeb455662c28c782562d1a23b24
Jacopo Dell'Oste <jacodell25@gmail.com>
2025-11-21 19:44:01 +0100

    style: removed ugly background color

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -12,7 +12,7 @@ function Movie() {
     } = useMoviesData();
 
     return (
-        <div className="movies-page-container min-h-screen bg-gray-900 pt-16">
+        <div className="movies-page-container min-h-screen pt-16">
             <CardGrid
                 title="Explore the Complete Movies Catalog"
                 data={discoverMovies}
