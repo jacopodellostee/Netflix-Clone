646c3a762a48decb6a71aea47d3ab85bcf4d7054
Matteo Paglietta <Matteo.Paglietta@edu-its.it>
2025-11-24 10:49:25 +0100

    style(homepage): removed useless comments

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,47 +1,45 @@
 import { useMoviesData } from '../providers/MoviesDataProvider';
 import Hero from '../components/Hero';
-import Carousel from '../components/Carousel'; 
+import Carousel from '../components/Carousel';
 
 function Homepage() {
-    const { 
-        topRatedMovies: movies, 
-        nowPlayingMovies: moviesNowPlaying, 
-        topRatedSeries: series, 
-        nowPlayingSeries: seriesNowPlaying, 
-        loading, 
-        error 
+    const {
+        topRatedMovies: movies,
+        nowPlayingMovies: moviesNowPlaying,
+        topRatedSeries: series,
+        nowPlayingSeries: seriesNowPlaying,
+        loading,
+        error
     } = useMoviesData();
 
     return (
         <div className="homepage">
 
             <Hero />
-
-            {/* film */}
             <div className="container-fluid">
-                
-                <Carousel 
+
+                <Carousel
                     title="Top Rated Movies"
                     data={movies}
                     loading={loading}
                     error={error}
                 />
 
-                <Carousel 
+                <Carousel
                     title="Recently Released Movies"
                     data={moviesNowPlaying}
                     loading={loading}
                     error={error}
                 />
 
-                <Carousel 
+                <Carousel
                     title="Top Rated Series Tv"
                     data={series}
                     loading={loading}
                     error={error}
                 />
 
-                <Carousel 
+                <Carousel
                     title="Recently Released Series Tv"
                     data={seriesNowPlaying}
                     loading={loading}
